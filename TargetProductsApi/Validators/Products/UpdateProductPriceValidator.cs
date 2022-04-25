a07f9b3d1d7dbbe0b187bca3962cb43e1a8e5fa7
Drew Churchill <dmc9870@gmail.com>
2022-04-25 12:43:11 -0500

    Add more validation for currency codes

--- TargetProductsApi/Validators/Products/UpdateProductPriceValidator.cs
+++ TargetProductsApi/Validators/Products/UpdateProductPriceValidator.cs
@@ -15,5 +15,20 @@ public static class UpdateProductPriceValidator
         {
             throw new ArgumentNullException(nameof(product));
         }
+
+        if (product.CurrentPrice is null)
+        {
+            throw new InvalidOperationException("Current price is required.");
+        }
+
+        if (string.IsNullOrEmpty(product.CurrentPrice.CurrencyCode))
+        {
+            throw new InvalidOperationException("CurrencyCode is required.");
+        }
+
+        if (product.CurrentPrice.CurrencyCode.Length != 3)
+        {
+            throw new InvalidOperationException("CurrencyCode must be three characters.");
+        }
     }
 }
